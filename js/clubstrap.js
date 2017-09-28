033c9ed5b0ea2015028e86a7632e2962a419a6ab
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2017-09-27 20:37:56 -0500

    Push footer to bottom based on docheight - Fixes #4

---
+++ js/clubstrap.js
@@ -0,0 +1,12 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  var docHeight = $(window).height();
+  var footerHeight = $('.footer').height();
+  var footerTop = $('.footer').position().top + footerHeight;
+  if (footerTop < docHeight) {
+   $('.footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
+  }
+
+})(jQuery, Drupal);
